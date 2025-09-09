713705c4882722f58eb22a8d957895d66a1bd430
Hadji Tejuco <hadji.tejuco@gmail.com>
2025-09-09 10:47:28 +0800

    up

---
+++ Module3/AdditionOperator.java
@@ -0,0 +1,8 @@
+package Module3;
+
+public class AdditionOperator {
+    public static void main(String[] args) {
+        int x = 20; int y = 15;
+        System.out.println("Sum = "+ (x+y));
+    }
+}
