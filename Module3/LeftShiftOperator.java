2ed61a865d4b3c86e0f4671193d44d2c6ad016e4
Hadji Tejuco <hadji.tejuco@gmail.com>
2025-09-09 11:24:43 +0800

    up

---
+++ Module3/LeftShiftOperator.java
@@ -0,0 +1,10 @@
+package Module3;
+//The left shift operator moves all bits by a given number of bits to the left.
+//<<
+public class LeftShiftOperator {
+    public static void main(String[] args) {
+        int a =5; //8421 - 0101 
+                  //8421 -  1010  
+        System.out.println("Left Shift "+ (a<<1));
+    }    
+}
