a13d188a10a6b2e95afca01757e09a63aedc5e2a
Hadji Tejuco <hadji.tejuco@gmail.com>
2025-09-09 11:47:49 +0800

    up

---
+++ Module3/CastOperator.java
@@ -0,0 +1,10 @@
+package Module3;
+
+public class CastOperator {
+    public static void main(String[] args) {
+         double a = 10.59;
+         int b = 5;
+         System.out.println("Cast " + (int) a);
+         System.out.println("Cast " + (double) b);
+    }
+}
