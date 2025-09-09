faa84b11185ffbf09401947b9fd6b9a5c199822d
Hadji Tejuco <hadji.tejuco@gmail.com>
2025-09-09 11:32:31 +0800

    up

---
+++ Module3/UnsignedRightShift.java
@@ -0,0 +1,9 @@
+package Module3;
+//>>>
+public class UnsignedRightShift {
+    public static void main(String[] args) {
+        int a = 8;  //8421.  1000
+                
+        System.out.println("Unsigned Right Shift "+ (a >>> 2));
+    }
+}
