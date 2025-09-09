40059855b1fe8a828189fe92601baebcf63446fc
Hadji Tejuco <hadji.tejuco@gmail.com>
2025-09-09 10:24:54 +0800

    up

---
+++ Main.java
@@ -0,0 +1,9 @@
+// Class definition
+public class Main {
+    // Main method: Entry point of the program
+    public static void main(String[] args) {
+        // Your code goes here
+        System.out.println("Hello, World!"); // Example: Printing a message
+    }
+}
+
