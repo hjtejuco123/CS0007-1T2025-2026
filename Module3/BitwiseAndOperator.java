ee6628cd8e83a7de1ac256ca9a39abf115ce2bf8
Hadji Tejuco <hadji.tejuco@gmail.com>
2025-09-09 10:56:33 +0800

    up

---
+++ Module3/BitwiseAndOperator.java
@@ -0,0 +1,13 @@
+package Module3;
+//& operator
+/*
+ * The bitwise AND (&) operator in Java is used to perform a bit-level operation between two integers. It compares each bit of its operands and returns 1 if both corresponding bits are 1; otherwise, it returns 0.
+ * 
+ */
+public class BitwiseAndOperator {
+    public static void main(String[] args) {
+        int a = 15; //8421 1111
+        int b = 7; //8421 1111
+        System.out.println("Bitwise AND (a&b) = "+ (a&b));
+    }
+}
