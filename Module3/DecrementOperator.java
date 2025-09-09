dbb27f8df7ddcb8df4077a85c355739dd3675efb
Hadji Tejuco <hadji.tejuco@gmail.com>
2025-09-09 10:43:58 +0800

    up

---
+++ Module3/DecrementOperator.java
@@ -0,0 +1,14 @@
+package Module3;
+
+public class DecrementOperator {
+    
+    public static void main(String[] args) {
+        int a = 5;
+        System.out.println("Decrement Operator!"); 
+        System.out.println("Initial Value "+a);
+        a--; //post Decrement
+        System.out.println("Post Decrement Value "+a);
+        --a; //pre Decrement
+        System.out.println("Pre Decrement Values "+a);
+    }
+}
