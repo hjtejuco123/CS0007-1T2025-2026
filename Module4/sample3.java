50406829ea1a7958e6978c23336243aea56ee41a
Hadji Tejuco <hadji.tejuco@gmail.com>
2025-09-16 10:49:08 +0800

    up

---
+++ Module4/sample3.java
@@ -0,0 +1,35 @@
+//create a program to determine the color B=Blue or b=blue, G=Green or g=green, R=Red or r=red, Y=Yellow or y=yellow
+//use switch case
+package Module4;
+import java.util.Scanner;
+public class sample3 {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Enter a color code (B/G/R/Y): ");
+        char colorCode = scanner.next().charAt(0);
+        String colorName;
+
+        switch (colorCode) {
+            case 'B':
+            case 'b':
+                colorName = "Blue";
+                break;
+            case 'G':
+            case 'g':
+                colorName = "Green";
+                break;
+            case 'R':
+            case 'r':
+                colorName = "Red";
+                break;
+            case 'Y':
+            case 'y':
+                colorName = "Yellow";
+                break;
+            default:
+                colorName = "Invalid color code";
+        }
+
+        System.out.println("The color is: " + colorName);
+    }
+}
