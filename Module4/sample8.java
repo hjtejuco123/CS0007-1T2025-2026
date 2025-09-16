97b66e0a58a77b3b4372705fb4b7c0cf25f19384
Hadji Tejuco <hadji.tejuco@gmail.com>
2025-09-16 11:24:58 +0800

    up

--- Module4/sample8.java
+++ Module4/sample8.java
@@ -5,19 +5,26 @@ package Module4;
 //or datatype[] arrayName = {value1, value2, value3, ...};
 public class sample8 {
     public static void main(String[] args) {
-        String[] studentNames = new String[5];
+        String[] studentNames = new String[10];
         studentNames[0] = "Alice";
         studentNames[1] = "Bob";
         studentNames[2] = "Charlie";
         studentNames[3] = "David";
         studentNames[4] = "Eva";
+        studentNames[5] = "Frank";
+        studentNames[6] = "Grace";
+        studentNames[7] = "Hannah";
+        studentNames[8] = "Ian";
+        studentNames[9] = "Jack";         
 
         System.out.println("Student Names:");
         for (String name : studentNames) {
             System.out.println(name);
         }
-        
-
-        
+        //use for loops to display the names
+        //           i<=4
+        for(int i=0; i<studentNames.length; i++){
+            System.out.println(studentNames[i]);    
+        }
     }
 }
