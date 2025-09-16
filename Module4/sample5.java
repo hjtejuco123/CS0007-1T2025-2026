f6528630140bd217683916feacc9121e024a29c8
Hadji Tejuco <hadji.tejuco@gmail.com>
2025-09-16 11:02:43 +0800

    up

---
+++ Module4/sample5.java
@@ -0,0 +1,17 @@
+package Module4;
+//create a program to count numbers form 1 to 10 using while loop
+//syntax while(condition){
+//statements
+//}
+import java.util.Scanner;
+public class sample5 {
+    public static void main(String[] args) {
+        int count = 1;
+
+        while (count <= 10) {
+            System.out.println(count);
+            count++;
+        }
+        System.out.println("Counting completed.");
+    }
+}
