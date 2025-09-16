abc02a3dd2c5c59e6581bb02f40b8eaff63adb71
Hadji Tejuco <hadji.tejuco@gmail.com>
2025-09-16 11:10:23 +0800

    up

---
+++ Module4/sample7.java
@@ -0,0 +1,13 @@
+package Module4;
+//create a program to count numbers form 1 to 10 using for loop
+//syntax for(initialization; condition; increment/decrement){
+//statements
+//} 
+public class sample7 {
+    public static void main(String[] args) {
+        for (int i = 1; i <= 10; i++) {
+            System.out.println(i);
+        }
+        System.out.println("Counting completed.");
+    }
+}
