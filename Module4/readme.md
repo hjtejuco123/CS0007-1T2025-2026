eeb5f29e8ef5419e99c497457e96998f49584b24
Hadji Tejuco <hadji.tejuco@gmail.com>
2025-09-16 10:38:20 +0800

    up

--- Module4/readme.md
+++ Module4/readme.md
@@ -1,1 +1,29 @@
 Module 4
+
+if, else statement
+switch, case statement
+while loop
+    
+    while(condition){
+
+
+    }
+
+
+do, while loop
+
+    do {
+
+
+    }while (condition);
+
+for loop
+
+
+
+Arrays
+    Single
+    Multi
+
+
+
---
+++ Module4/sample1.java
@@ -0,0 +1,25 @@
+//Create a program to determine the student grades 
+package Module4;
+import java.util.Scanner;
+public class sample1 {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Enter the student's score: ");
+        int score = scanner.nextInt();
+        char grade;
+
+        if (score >= 90) {
+            grade = 'A';
+        } else if (score >= 80) {
+            grade = 'B';
+        } else if (score >= 70) {
+            grade = 'C';
+        } else if (score >= 60) {
+            grade = 'D';
+        } else {
+            grade = 'F';
+        }
+
+        System.out.println("The student's grade is: " + grade);
+    }
+}
