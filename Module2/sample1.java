04a1267913ca01b603d3e29b546065863a3958dc
Hadji Tejuco <hadji.tejuco@gmail.com>
2025-09-17 11:02:02 +0800

    Update sample1.java

---
+++ Module2/sample1.java
@@ -0,0 +1,8 @@
+package Module2;
+//print hello world
+public class sample1 {
+    public static void main(String[] args) {
+        System.out.println("Hello World");
+        System.out.println("Welcome to Java Programming");
+    }
+}
