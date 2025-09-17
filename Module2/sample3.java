a19e12ee17e2ad0171cbc0cae9d00d68d182dd91
Hadji Tejuco <hadji.tejuco@gmail.com>
2025-09-17 11:22:39 +0800

    up

---
+++ Module2/sample3.java
@@ -0,0 +1,25 @@
+package Module2;
+//give examples of use of different data types
+
+public class sample3 {
+    public static void main(String[] args) {
+        int myInt = 10; // integer data type
+        double myDouble = 20.5; // double data type
+        char myChar = 'A'; // character data type
+        boolean myBoolean = true; // boolean data type
+        String myString = "Hello, Java!"; // String data type
+
+        System.out.println("Integer: " + myInt);
+        System.out.println("Double: " + myDouble);
+        System.out.println("Character: " + myChar);
+        System.out.println("Boolean: " + myBoolean);
+        System.out.println("String: " + myString);
+
+        // Using printf to format output
+        System.out.printf("Formatted Integer: %d%n", myInt);
+        System.out.printf("Formatted Double: %.2f%n", myDouble);
+        System.out.printf("Formatted Character: %c%n", myChar);
+        System.out.printf("Formatted Boolean: %b%n", myBoolean);
+        System.out.printf("Formatted String: %s%n", myString);
+    }    
+}
