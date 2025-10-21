d05b2a99d523a9b3a497444cd406a5f383d8a8f8
Hadji Tejuco <hadji.tejuco@gmail.com>
2025-10-21 12:28:55 +0800

    Add files via upload

---
+++ Module78/SimpleAssertionExample.java
@@ -0,0 +1,15 @@
+
+package Module78;
+
+
+public class SimpleAssertionExample {
+    public static void main(String[] args) {
+        int age = -5;
+        
+        assert age > 0;
+        
+        System.out.println("Age is valid "+age);
+    }
+    
+    
+}
