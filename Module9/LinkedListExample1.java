60cb406541e758686c0c951b99d0094735d1d6e1
Hadji Tejuco <hadji.tejuco@gmail.com>
2025-11-02 14:20:34 +0800

    Create LinkedListExample1.java

---
+++ Module9/LinkedListExample1.java
@@ -0,0 +1,14 @@
+//LinkedList Examples
+//Example 1: Adding Elements
+
+import java.util.LinkedList;
+
+public class LinkedListExample1 {
+    public static void main(String[] args) {
+        LinkedList<String> names = new LinkedList<>();
+        names.add("Alice");
+        names.add("Bob");
+        names.add("Charlie");
+        System.out.println("Names: " + names);
+    }
+}
