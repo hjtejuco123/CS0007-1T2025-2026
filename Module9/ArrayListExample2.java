4717b0a799f7277d39697cb3526882599dc63b47
Hadji Tejuco <hadji.tejuco@gmail.com>
2025-11-02 14:17:57 +0800

    Add ArrayListExample2 to demonstrate ArrayList usage

---
+++ Module9/ArrayListExample2.java
@@ -0,0 +1,12 @@
+import java.util.ArrayList;
+
+public class ArrayListExample2 {
+    public static void main(String[] args) {
+        ArrayList<Integer> numbers = new ArrayList<>();
+        numbers.add(10);
+        numbers.add(20);
+        numbers.add(30);
+        numbers.remove(Integer.valueOf(20)); // Remove by value
+        System.out.println("Numbers after removal: " + numbers);
+    }
+}
